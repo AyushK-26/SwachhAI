9064a1fe6adb7520045b029aa6f579d02f34558e
Ayush Khanduri <ayushkhanduri2612@gmail.com>
2025-09-13 12:28:21 +0530

    Initial commit

---
+++ server/server.mjs
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+import cors from "cors";
+import { corsOptions } from "./config/corsOptions.mjs";
+
+const PORT = process.env.PORT || 3500;
+
+app.use(cors(corsOptions));
+
+app.post("/report", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
